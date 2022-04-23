9de5542e3599d3576c0095310d61cc17837a04ab
Tom Riley <tom.riley@outlook.com>
2022-04-23 21:27:04 +0100

    updating urls

--- Channels/views.py
+++ Channels/views.py
@@ -4,9 +4,26 @@ from .models import *
 from .serializers.common import *
 
 
-class ChannelList(ListCreateAPIView):  # lists all gamechannels across site
+# lists all gamechannels, create new game channels across site
+class ChannelList(ListCreateAPIView):
     queryset = Channel.objects.all()
     serializer_class = ChannelSerializer
+
+
+class SingleChannel(RetrieveAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+
+# class SingleChannelName(RetrieveAPIView):
+#     serializer_class = ChannelSerializer
+
+#     def get_queryset(self, request, urlName):
+#         queryset = Channel.objects.all()
+#         # urlName = self.request.query_params.get()
+#         if urlName:
+#             queryset = queryset.filter(urlName=urlName)
+#         return queryset
 
 
 class ChannelUpdateDestroy(RetrieveUpdateDestroyAPIView):
